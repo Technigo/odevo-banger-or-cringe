d90cc7c14832f29fd6458f3c2a2a17de3d397ccb
Jennie Dalgren <jennie.dalgren@gmail.com>
2026-05-28 10:49:09 +0200

    add post endpoint

--- backend/routes/songRoutes.js
+++ backend/routes/songRoutes.js
@@ -9,4 +9,14 @@ router.get("/", (req, res) => {
     .catch(err => res.status(500).json({ error: err.message }))
 })
 
+router.post("/", (req, res) => {
+  const { title, artist, spotifyUrl } = req.body
+  const song = new Song({ title, artist, spotifyUrl })
+  song.save().then((savedSong) => {
+    res.status(201).json(savedSong)
+  }).catch((error) => {
+    res.status(400).json({ error: "Could not save song" })
+  })
+})
+
 export default router
